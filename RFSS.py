f1955670dc5d46cb346176ef15012bdb58e8fffc
jmjugar <rfims.rfims@gmail.com>
2023-08-23 16:14:00 +0000

    Added schedule_log write to mongoDB

--- RFSS.py
+++ RFSS.py
@@ -7,6 +7,12 @@ import csv
 import RFSS_Autonomous
 import logging
 import json
+from pymongo import MongoClient
+
+# Connect to MongoDB
+client = MongoClient('localhost', 27017)
+db = client['status_db']
+schedule_collection = db['schedule_log']
 
 # Reset the Root Logger - this section is used to reset the root logger and then apply below configuration
 for handler in logging.root.handlers[:]:
@@ -33,25 +39,30 @@ def fetchReport():
         if response.status == 200:
             data = json.loads(response.read().decode())["list"][:-1]
 
-            # Get the current day of the week and add 1 for tomorrow
+            # Get the current day of the week and initialize an empty list to put the response in
             today = datetime.datetime.utcnow().weekday()
-
             rows = []
 
+            # Response received -> define the data
             for entry in data:
                 satellite = entry[1].replace(" ", "-")
                 aos_time = datetime.datetime.utcfromtimestamp(entry[2])
                 los_time = datetime.datetime.utcfromtimestamp(entry[3])
                 day_of_week = aos_time.weekday()
                 max_elevation = entry[7]
-
+                
+                # Make sure to drop any items in the schedule that are not today and that are above minElevation
+                # We do this since teh schedule will provide a couple of days.  Since we are doing this daily
+                # we restrict the schedule to avoid duplicates 
                 if day_of_week == today and max_elevation > minElevation:
                     formatted_aos = f"({aos_time.hour},{aos_time.minute},{aos_time.second})"
                     formatted_los = f"({los_time.hour},{los_time.minute},{los_time.second})"
                     rows.append((aos_time, day_of_week, formatted_aos, formatted_los, satellite, max_elevation))
 
+            # Sort the schedule list
             rows.sort(key=lambda x: x[0])
 
+            # Write the scehdule to csv to use in RFSS_Autonomous.main()
             output_path = "/home/noaa_gms/RFSS/Tools/Report_Exports/schedule.csv"
             with open(output_path, 'w', newline='') as file:
                 writer = csv.writer(file)
@@ -59,7 +70,26 @@ def fetchReport():
                 for index, row in enumerate(rows, start=1):
                     writer.writerow([index, row[1], row[2], row[3], row[4], row[5]])
 
-            logging.info('New schedule in use extracted and ready for use.')
+            # Additionally add the schedule with write timestamp to mongoDB for use later 
+            file_path = "/home/noaa_gms/RFSS/Tools/Report_Exports/schedule.csv"
+            with open(file_path, newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                rows = [row for row in reader]
+
+                # Convert AOS and LOS into proper tuples
+                for row in rows:
+                    row['AOS'] = tuple(map(int, row['AOS'][1:-1].split(',')))
+                    row['LOS'] = tuple(map(int, row['LOS'][1:-1].split(',')))
+                    row['MaxElevation'] = float(row['MaxElevation'])
+
+                # Insert the data into MongoDB as a single document
+                document = {
+                    "timestamp": datetime.datetime.utcnow(),
+                    "schedule": rows,
+                    }
+                schedule_collection.insert_one(document)
+
+            logging.info('New schedule extracted, logged and ready for use.')
             RFSS_Autonomous.main()
         
     except Exception as e:
