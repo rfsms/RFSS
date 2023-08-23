bb546ceadbd07532484938c8938d20c25219744c
jmjugar <rfims.rfims@gmail.com>
2023-08-23 21:56:56 +0000

    Working Dashboard for daily events + selection

---
+++ Dashboard/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import pymongo
+import datetime
+from pytz import timezone
+
+app = Flask(__name__)
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["status_db"]
+collection = db["schedule_log"]
+
+def format_time(time_tuple):
+    return f"{time_tuple[0]:02d}:{time_tuple[1]:02d}:{time_tuple[2]:02d}"
+
+def convert_to_EST(time_str):
+    hours, minutes, seconds = map(int, time_str.split(':'))
+    utc_time = datetime.time(hours, minutes, seconds)
+    utc_dt = datetime.datetime.combine(datetime.date.today(), utc_time, tzinfo=timezone('UTC'))
+    est_dt = utc_dt.astimezone(timezone('US/Eastern'))
+    return est_dt.strftime('%H:%M:%S')
+
+@app.route('/')
+def calendar():
+    current_utc_date = datetime.datetime.utcnow().strftime("%m/%d/%Y %H:%M")
+    today_date_obj = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+    next_day_date_obj = today_date_obj + datetime.timedelta(days=1)
+    event = collection.find_one({"timestamp": {"$gte": today_date_obj, "$lt": next_day_date_obj}})
+    if event:
+        for item in event['schedule']:
+            item['AOS'] = format_time(item['AOS'])
+            item['LOS'] = format_time(item['LOS'])
+            item['AOS_EST'] = convert_to_EST(item['AOS'])
+            item['LOS_EST'] = convert_to_EST(item['LOS'])
+    return render_template('calendar.html', current_time=current_utc_date, event=event['schedule'] if event else None)
+
+@app.route('/events', methods=['POST'])
+def events():
+    selected_date = request.form['date']
+    selected_date_obj = datetime.datetime.strptime(selected_date, '%Y-%m-%d')
+    next_day_date_obj = selected_date_obj + datetime.timedelta(days=1)
+    event = collection.find_one({
+        "timestamp": {
+            "$gte": selected_date_obj,
+            "$lt": next_day_date_obj
+        }
+    })
+    print(event)
+    if event:
+        for item in event['schedule']:
+            item['AOS'] = format_time(item['AOS'])
+            item['LOS'] = format_time(item['LOS'])
+            item['AOS_EST'] = convert_to_EST(item['AOS'])
+            item['LOS_EST'] = convert_to_EST(item['LOS'])
+    current_utc_time = datetime.datetime.utcnow().strftime("%m/%d/%Y %H:%M")
+    return render_template('events.html', current_time=current_utc_time, event=event if event else None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
