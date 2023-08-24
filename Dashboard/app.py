f784ad66be8e6efe35119b0e55a5f34f31a8c39c
jmjugar <rfims.rfims@gmail.com>
2023-08-24 16:45:05 +0000

    more row highligiting, with current and next

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -24,10 +24,10 @@ def calendar():
     today_date_obj = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
     next_day_date_obj = today_date_obj + datetime.timedelta(days=1)
     event = collection.find_one({"timestamp": {"$gte": today_date_obj, "$lt": next_day_date_obj}})
-
     if event:
         date_from_db = event['timestamp']
         date_from_db_str = date_from_db.strftime('%Y-%m-%d')
+        next_event_found = False
         for item in event['schedule']:
             AOS_time_str = ":".join(map(str, item['AOS']))
             LOS_time_str = ":".join(map(str, item['LOS']))
@@ -37,8 +37,13 @@ def calendar():
             AOS_datetime = date_from_db.replace(hour=AOS_time.hour, minute=AOS_time.minute, second=AOS_time.second)
             LOS_datetime = date_from_db.replace(hour=LOS_time.hour, minute=LOS_time.minute, second=LOS_time.second)
 
-            item['highlight'] = AOS_datetime <= current_utc_time <= LOS_datetime
-            print(item['highlight'] )
+            if AOS_datetime <= current_utc_time <= LOS_datetime:
+                item['highlight'] = "blue"
+            elif AOS_datetime > current_utc_time and not next_event_found:
+                item['highlight'] = "yellow"
+                next_event_found = True
+            else:
+                item['highlight'] = ""
 
             item['AOS'] = AOS_time_str
             item['LOS'] = LOS_time_str
@@ -47,7 +52,6 @@ def calendar():
 
     current_utc_time_str = current_utc_time.strftime("%m/%d/%Y %H:%M")
     return render_template('calendar.html', current_utc_time=current_utc_time_str, event=event['schedule'] if event else None, date_from_db=date_from_db_str)
-
 
 
 @app.route('/events', methods=['POST'])
