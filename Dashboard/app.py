0c004542d2cce8635fad82479ac86ba6ca031cd7
jmjugar <rfims.rfims@gmail.com>
2023-08-24 15:57:47 +0000

    Dashboard current event highlights

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -20,17 +20,35 @@ def convert_to_EST(time_str):
 
 @app.route('/')
 def calendar():
-    current_utc_date = datetime.datetime.utcnow().strftime("%m/%d/%Y %H:%M")
+    current_utc_time = datetime.datetime.utcnow()
     today_date_obj = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
     next_day_date_obj = today_date_obj + datetime.timedelta(days=1)
     event = collection.find_one({"timestamp": {"$gte": today_date_obj, "$lt": next_day_date_obj}})
+
     if event:
+        date_from_db = event['timestamp']
+        date_from_db_str = date_from_db.strftime('%Y-%m-%d')
         for item in event['schedule']:
-            item['AOS'] = format_time(item['AOS'])
-            item['LOS'] = format_time(item['LOS'])
+            AOS_time_str = ":".join(map(str, item['AOS']))
+            LOS_time_str = ":".join(map(str, item['LOS']))
+            AOS_time = datetime.datetime.strptime(AOS_time_str, '%H:%M:%S')
+            LOS_time = datetime.datetime.strptime(LOS_time_str, '%H:%M:%S')
+
+            AOS_datetime = date_from_db.replace(hour=AOS_time.hour, minute=AOS_time.minute, second=AOS_time.second)
+            LOS_datetime = date_from_db.replace(hour=LOS_time.hour, minute=LOS_time.minute, second=LOS_time.second)
+
+            item['highlight'] = AOS_datetime <= current_utc_time <= LOS_datetime
+            print(item['highlight'] )
+
+            item['AOS'] = AOS_time_str
+            item['LOS'] = LOS_time_str
             item['AOS_EST'] = convert_to_EST(item['AOS'])
             item['LOS_EST'] = convert_to_EST(item['LOS'])
-    return render_template('calendar.html', current_time=current_utc_date, event=event['schedule'] if event else None)
+
+    current_utc_time_str = current_utc_time.strftime("%m/%d/%Y %H:%M")
+    return render_template('calendar.html', current_utc_time=current_utc_time_str, event=event['schedule'] if event else None, date_from_db=date_from_db_str)
+
+
 
 @app.route('/events', methods=['POST'])
 def events():
@@ -43,15 +61,16 @@ def events():
             "$lt": next_day_date_obj
         }
     })
-
     if event:
+        date_from_db = event['timestamp']
+        date_from_db_str = date_from_db.strftime('%Y-%m-%d') 
         for item in event['schedule']:
             item['AOS'] = format_time(item['AOS'])
             item['LOS'] = format_time(item['LOS'])
             item['AOS_EST'] = convert_to_EST(item['AOS'])
             item['LOS_EST'] = convert_to_EST(item['LOS'])
     current_utc_time = datetime.datetime.utcnow().strftime("%m/%d/%Y %H:%M")
-    return render_template('events.html', current_time=current_utc_time, event=event if event else None)
+    return render_template('events.html', event=event if event else None, selected_date=selected_date, date_from_db=date_from_db_str)
 
 if __name__ == '__main__':
     app.run(debug=True)
