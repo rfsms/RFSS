7b5c67db7cbda8e555842d1c2ae2e55d5359bdc0
jmjugar <rfims.rfims@gmail.com>
2023-10-17 18:37:37 +0000

    integrating spectrograms with MXG

--- Tools/Testing/MXG_WFM_Setup.py
+++ Tools/Testing/MXG_WFM_Setup.py
@@ -1,10 +1,22 @@
 import pyvisa
 import time
+import csv
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
 PXA_RESOURCE_STRING = 'TCPIP::192.168.2.101::hislip0::INSTR' 
 MXG_RESOURCE_STRING = 'TCPIP::192.168.130.66::5025::SOCKET' 
 RM = pyvisa.ResourceManager()
-INSTR_DIR = 'D:\\Users\\Instrument\\Documents\\BASIC\\data\\WAV\\results\\RFSS\\'
+# INSTR_DIR = 'D:\\Users\\Instrument\\Documents\\BASIC\\data\\WAV\\results\\RFSS\\'
+# TRACE_DIR = 'D:\\Users\\Instrument\\Documents\\SA\\data\\traces\\RFSS\\'
+
+# Specify the number of times you want to capture the trace
+num_capture_iterations = 100
+
+# Initialize a list to store data from each iteration and timestamps
+data_iterations = []
+timestamp_iterations = []
 
 # pyvisa.log_to_screen()
 
@@ -15,7 +27,6 @@ waveforms_15MHz = [
 "2-3-15MHZ_SCS15KHZ_9UES_ALLTTI_ALLCP_ALLPUSCH_1698_QPSK_3PRB_GUARDBAND.ARB",
 "4-15MHZ_SCS15KHZ_9UES_ALLTTI_ALLDFT_ALLPUSCH_1698_QPSK.ARB",
 "6-15MHZ_SCS15KHZ_9UES_ALLTTI_ALLCP_ALLPUSCH_1702_QPSK.ARB",
-"16-1-15MHZ_SCS15KHZ_9UES_ALLTTI_ALLCP_ALLPUSCH_1702_QPSK_1PRB_GUARDBAND.ARB",
 "6-2-15MHZ_SCS15KHZ_9UES_ALLTTI_ALLCP_ALLPUSCH_1702_QPSK_2PRB_GUARDBAND.ARB",
 "6-3-15MHZ_SCS15KHZ_9UES_ALLTTI_ALLCP_ALLPUSCH_1702_QPSK_3PRB_GUARDBAND.ARB",
 "7-15MHZ_SCS15KHZ_9UES_ALLTTI_ALLDFT_ALLPUSCH_1702_QPSK.ARB",
@@ -55,12 +66,7 @@ waveforms_15MHz = [
 # "21-5MHZ_SCS15KHZ_3UES_ALLTTI_ALLDFT_ALLPUSCH_QPSK_NONBLANKED.ARB"
 # ]
 
-# with RM.open_resource(PXA_RESOURCE_STRING, timeout = 2000) as PXA:
-#     # PXA reset/setup
-#     pxa_idn = PXA.query('*IDN?')
-#     print(f'PXA: {pxa_idn}')
-
-with RM.open_resource(MXG_RESOURCE_STRING, timeout=15000) as MXG:
+with RM.open_resource(MXG_RESOURCE_STRING, timeout=2000) as MXG:
     MXG.read_termination = '\n'
     MXG.write_termination = '\n'
     
@@ -81,9 +87,6 @@ with RM.open_resource(MXG_RESOURCE_STRING, timeout=15000) as MXG:
     power = MXG.query_ascii_values(':POW?')
     # print(f'Power: {power}')
 
-    MXG.write(':RAD:ARB 1')
-    MXG.write(':OUTP 1')
-
     # Extract waveform names from the response
     for entry in waveforms_15MHz:
         # print(f'setting up now and sending "{entry}"')
@@ -101,8 +104,36 @@ with RM.open_resource(MXG_RESOURCE_STRING, timeout=15000) as MXG:
         if MXG.query('*OPC?') == '1':
             MXG.write(':RAD:ARB 1')
             MXG.write(':OUTP 1')
-            time.sleep(1)
-            continue
+            time.sleep(2)
+            # input('Waiting for user confirmation')
+            with RM.open_resource(PXA_RESOURCE_STRING, timeout = 2000) as PXA:
+                # PXA reset/setup
+                PXA.write('*RST')
+                PXA.write('*CLS')
+                PXA.write('SYST:DEF SCR')
+                pxa_idn = PXA.query('*IDN?')
+                print(f'PXA: {pxa_idn}')
+                PXA.write("INIT:CONT ON")
+                PXA.write('SENS:FREQ:CENT 1702500000')
+                PXA.write('FREQ:SPAN 20000000')
+                PXA.write('DISP:WIND:TRAC:Y:RLEV -40')
+                PXA.write('POW:ATT 0')
+                PXA.write('POW:GAIN ON')
+                PXA.write('BAND:RES 1000')
+
+                PXA.write(":INST:NSEL 8")
+                PXA.write(":CONF:WAV")
+                PXA.write(":INIT:CONT OFF")
+                PXA.write('SENS:FREQ:CENT 1702500000')
+                PXA.write('POW:ATT:AUTO OFF')
+                PXA.write('POW:ATT 0')
+                PXA.write('DISP:WAV:VIEW:NSEL 1')
+                PXA.write('POW:GAIN ON')
+                PXA.write('WAV:SRAT 18.75MHz')
+                PXA.write('WAV:SWE:TIME 16ms')
+                PXA.write('DISP:WAV:VIEW:WIND:TRAC:Y:COUP ON')
+                PXA.write(":FORM:BORD SWAP")
+                PXA.write(":FORM REAL,32")
         else:
             print("Operation issue")
             break        
