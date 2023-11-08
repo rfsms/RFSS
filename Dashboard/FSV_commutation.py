c682c0ccf2990380669b1faabbf95aaf624d1526
jmjugar <rfims.rfims@gmail.com>
2023-11-08 18:31:37 +0000

    cleanup + adding a *wai to ensure new data

--- Dashboard/FSV_commutation.py
+++ Dashboard/FSV_commutation.py
@@ -117,7 +117,6 @@ def instrument_scanning_setup():
 
 def instrument_commutation_setup(center_frequency_MHz, span_MHz, points):
     try:
-
         FSV.visa_timeout = 5000
         FSV.write("INST:SEL 'Spectrum'")
         # FSV.write("CALC1:SGR:STAT OFF")
@@ -128,15 +127,14 @@ def instrument_commutation_setup(center_frequency_MHz, span_MHz, points):
         FSV.write("INST IQ")
         FSV.write(f"SENS:FREQ:CENT {center_frequency_MHz}MHz")
         FSV.write("INST:SEL 'Spectrum'")
-
-    except KeyboardInterrupt:
-        print(f"An error occurred in instrument setup")
+    except Exception as e:
+        logging.info(f"An error occurred in instrument setup: {e}")
 
 # def captureTrace(set_az):
 def captureTrace(iq, set_az, band):
     try:
         FSV.write("INST:SEL 'Spectrum'")
-        FSV.write("INIT:IMM")
+        FSV.write("INIT:IMM;*WAI")
         opc_result = FSV.query('*OPC?')
         if opc_result != '1':
             logging.error(f"Operation not complete (*OPC? returned '{opc_result}')")
@@ -155,7 +153,7 @@ def captureTrace(iq, set_az, band):
                 logging.error(f"Invalid band selection: {band}")
                 return None
 
-            FSV.write("INIT:IMM")  # Small delay to allow instrument to process previous commands
+            FSV.write("INIT:IMM;*WAI")
             opc_result_iq = FSV.query('*OPC?')
             if opc_result_iq != '1':
                 logging.error(f"IQ data not stored (*OPC? returned '{opc_result_iq}' after setting to IQ)")
